14cf7c3999b23b79c32873184b26e1b9acf933f3
WanxiaJaneYang <1016887513@qq.com>
2023-12-07 12:14:06 +1030

    changed the login url

--- src/api/login/index.js
+++ src/api/login/index.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 
 const API_URL=process.env.REACT_APP_BASE_API_URL;
-const USER_URL=API_URL+"/users";
+// const USER_URL=API_URL+"/users";
+const USER_URL=API_URL+"/login";
 
 export const checkLoginInfo=async (student_id,password)=>{
 	try{
 		const body={
+			student_id:student_id,//commented out for testing
 			password:password
 		};
 
-		const response = await axios.post(USER_URL+"/"+student_id+"/login",body);
+		// const response = await axios.post(USER_URL+"/"+student_id+"/login",body);
+		const response = await axios.post(USER_URL,body);
 		if(response.status === 200){
 			localStorage.setItem("student_id",student_id);
 			return true;
